6fcec1a5738fd114d2d187f41d7be32ba7a1a89d
Pankaj <pankajbeniwal3112@gmail.com>
2023-07-02 19:26:48 +0530

    Set up Database Mongodb

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
+//Express server
 const express = require('express');
 const app = express();
 const port = 8000;
 
+//...Models....
+//Database
+const db = require('./config/mongoose');
+
+//...Views....
+//Layouts
 const expressLayouts = require('express-ejs-layouts');
 app.use(expressLayouts);
 
+//Assets
 app.use(express.static('./assets'));
-
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use express router
-app.use('/', require('./routes'));
-
-// set up the view engine
+//View Engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+//...Controller....
+//Router
+app.use('/', require('./routes'));
+
 
 
 
