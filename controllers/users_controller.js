d0eb03c3a7653cc8e34e7612cfff96444848cc21
Pankaj <pankajbeniwal3112@gmail.com>
2023-07-05 20:32:29 +0530

    add sign up page and authorize user

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,51 @@
-module.exports.profile = function(req, res){
-    res.render('users', {
-        title: "User Profile"
+const User = require('../model/user');
+
+//Fetch veiws and render them
+module.exports.signUp = function(req, res){
+    return res.render('user_signup', {
+        title: "Codeial | Sign Up"
     });
-};
+}
+
+
+module.exports.signIn = function(req, res){
+    console.log(req.cookie);
+    res.cookie('user_id', 25);
+    return res.render('user_signin', {
+        title: "Codeial | Sign In"
+    });
+}
+
+
+//Get the sign up data
+module.exports.create = function(req, res){
+    //if password and confirm password are not same then redirect back
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    //if password and confirm password are same then check if user already exists
+    User.findOne({email: req.body.email})
+    .then(function(user){
+        //if user does not exist then create user
+        if(!user){
+            User.create(req.body)
+            .then(function(user){
+                return res.redirect('/users/sign-in');
+            })
+            .catch(function(err){
+                console.log('Error in creating user while signing up', err);
+                return;
+            })
+        }
+        else{
+            res.redirect('back');
+        }
+    })
+    .catch((err) => console.log('Error something went wrong', err));
+}
+
+//Get the sign in data
+module.exports.createSession = function(req, res){
+    //TODO later
+}
--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const postsController = require('../controllers/posts_controller');
-
-router.get('/', postsController.home);
-router.get('/like', postsController.like);
-
-module.exports = router;
