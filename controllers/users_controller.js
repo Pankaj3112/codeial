14d9ecd17eada4dc6443468025c81406240c3e4c
Pankaj <pankajbeniwal3112@gmail.com>
2023-07-06 14:23:43 +0530

    add manual authentication and authorization

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,21 @@
 const User = require('../model/user');
 
 //Fetch veiws and render them
+module.exports.profile = function(req, res){
+
+    User.findOne({_id: req.cookies.user_id})
+    .then(function(user){
+        return res.render('user_profile', {
+            title: "Codeial | Profile",
+            user: user
+        });
+    })
+    .catch(function(err){
+        console.log('Error in finding user while rendering profile', err);
+        return res.redirect('/users/sign-in');
+    });    
+}
+
 module.exports.signUp = function(req, res){
     return res.render('user_signup', {
         title: "Codeial | Sign Up"
@@ -9,8 +24,6 @@ module.exports.signUp = function(req, res){
 
 
 module.exports.signIn = function(req, res){
-    console.log(req.cookie);
-    res.cookie('user_id', 25);
     return res.render('user_signin', {
         title: "Codeial | Sign In"
     });
@@ -47,5 +60,28 @@ module.exports.create = function(req, res){
 
 //Get the sign in data
 module.exports.createSession = function(req, res){
-    //TODO later
+    //find user
+    User.findOne({email: req.body.email})
+    .then(function(user){
+        //handle user not found and password not match
+        if(!user || req.body.password != user.password){
+            return res.redirect('back');
+        }
+        
+        //handle session creation and redirect to profile page
+        res.cookie('user_id', user.id);
+        return res.redirect('/users/profile');
+    })
+    // handle error
+    .catch(function(err){
+        console.log('Error in finding user while signing in', err);
+        return res.redirect('back');
+    });
+}
+
+
+module.exports.destroySession = function(req, res){
+    //handle session destroy and redirect to home page
+    res.clearCookie('user_id');
+    return res.redirect('/');
 }
